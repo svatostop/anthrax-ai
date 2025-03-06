2cb48d3cbb167378e98910599351a8cb84b3feab
xd <dhhugtu2@gmail.com>
2025-03-06 23:55:08 +0300

    Refactor Singleton to follow C++ best practices

--- engine/code/include/anthraxAI/utils/singleton.h
+++ engine/code/include/anthraxAI/utils/singleton.h
@@ -1,18 +1,22 @@
 #pragma once
 
+#include <memory>
+#include <type_traits>
+
 namespace Utils
 {
     template <typename T>
     class Singleton 
     {
-        private:
         protected:
-            Singleton() {}
+            Singleton() = default;
 
         public:
+            static_assert(std::is_base_of_v<Singleton<T>, T>, "T must inherit from Singleton");
+
             Singleton(const Singleton* obj) = delete;
             Singleton* operator=(const Singleton*) = delete; 
 
-            static T* GetInstance() {  static T Instance; return &Instance; }
+            static T* GetInstance() {  static T Instance; return std::addressof(Instance); }
     };
 }
