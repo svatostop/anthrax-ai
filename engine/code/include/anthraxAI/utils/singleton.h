56564ee016a8a70e7e31f7358607dfd1bf989ed7
xd <dhhugtu2@gmail.com>
2025-03-07 21:02:52 +0300

    Fix incomplete type check in Singleton by moving static_assert to GetInstance

--- engine/code/include/anthraxAI/utils/singleton.h
+++ engine/code/include/anthraxAI/utils/singleton.h
@@ -12,11 +12,15 @@ namespace Utils
             Singleton() = default;
 
         public:
-            static_assert(std::is_base_of_v<Singleton<T>, T>, "T must inherit from Singleton");
 
             Singleton(const Singleton* obj) = delete;
             Singleton* operator=(const Singleton*) = delete; 
 
-            static T* GetInstance() {  static T Instance; return std::addressof(Instance); }
+            static T* GetInstance()
+	    {
+		static_assert(std::is_base_of_v<Singleton<T>, T>, "T must inherit from Singleton");
+		static T Instance;
+		return std::addressof(Instance);
+	    }
     };
 }
